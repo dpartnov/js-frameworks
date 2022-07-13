342883c2961890c8d4fc99b7325ef2bc9dc34b17
Denys Partnov <dpartnov@outlook.com>
2022-07-13 21:25:41 +0200

    SQL data model and initial data inserts

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE FRAMEWORK_VERSIONS IF EXISTS;
+DROP TABLE FRAMEWORKS IF EXISTS;
+
+CREATE TABLE FRAMEWORKS (
+    ID bigint auto_increment primary key,
+    NAME varchar(30) not null,
+    DEPRECATION_DATE date not null,
+    HYPE_LEVEL integer not null
+);
+
+CREATE TABLE FRAMEWORK_VERSIONS (
+    ID integer auto_increment primary key,
+    FRAMEWORK_ID bigint not null,
+    VERSION varchar(15) not null,
+    foreign key (FRAMEWORK_ID) references FRAMEWORKS(ID)
+);
+
+
+
+
