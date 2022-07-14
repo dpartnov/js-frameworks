3351d8933834d497beffe65a647ec685e7e4a2ba
Denys Partnov <dpartnov@outlook.com>
2022-07-14 17:26:30 +0200

    SQL data model refactoring

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,14 +4,14 @@ DROP TABLE FRAMEWORKS IF EXISTS;
 CREATE TABLE FRAMEWORKS (
     ID bigint auto_increment primary key,
     NAME varchar(30) not null,
-    DEPRECATION_DATE date not null,
     HYPE_LEVEL integer not null
 );
 
 CREATE TABLE FRAMEWORK_VERSIONS (
-    ID integer auto_increment primary key,
+    ID bigint auto_increment primary key,
     FRAMEWORK_ID bigint not null,
     VERSION varchar(15) not null,
+    DEPRECATION_DATE date not null,
     foreign key (FRAMEWORK_ID) references FRAMEWORKS(ID)
 );
 
